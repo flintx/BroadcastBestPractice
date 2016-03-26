aa03567e6fbaf96d16b0c3c3d39b4f92a05a3d1b
Flintx <940852578@qq.com>
2016-03-26 16:25:41 +0800

    Initial commit

---
+++ app/src/main/java/com/flint/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.flint.broadcastbestpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
---
+++ app/src/main/java/com/flint/broadcastbestpractice/MainActivity.java
@@ -0,0 +1,24 @@
+package com.flint.broadcastbestpractice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.flint.broadcastbestpractice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+
+            }
+        });
+    }
+}
