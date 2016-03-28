76555a98585d1039fe96a8f870c03f84675a157e
Flintx <940852578@qq.com>
2016-03-28 19:15:58 +0800

    add remember_pass checkBox

--- app/src/main/java/com/flint/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/flint/broadcastbestpractice/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.flint.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,27 +14,66 @@ import android.widget.Toast;
  * Created by Administrator on 2016/3/26.
  */
 public class LoginActivity extends BaseActivity {
+
+    private SharedPreferences.Editor editor;
+
+    private SharedPreferences preferences;
+
     private EditText accountEdit;
+
     private EditText passwordEdit;
+
     private Button login;
+
+    private CheckBox rememberPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+
+        boolean isRemember = preferences.getBoolean("remember_password", false);
+
+        if (isRemember) {
+            String account = preferences.getString("account", "");
+            String password = preferences.getString("password", "");
+
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+
+            rememberPass.setChecked(true);
+        }
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
+
                 if (account.equals("admin") && password.equals("123456")) {
+
+                    editor = preferences.edit();
+
+                    if (rememberPass.isChecked()) {
+
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.commit();
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
+
                 } else {
                     Toast.makeText(LoginActivity.this, "account or password is invalid",
                             Toast.LENGTH_SHORT).show();
